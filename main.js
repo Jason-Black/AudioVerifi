e0e599f128235edbaad00623ec6e964a610ba3de
Jason-Black <71369201+Jason-Black@users.noreply.github.com>
2024-07-21 00:19:42 +0000

    update javascript

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-//GSAP ANIMATIONS
 import { gsap } from 'gsap';
 
 document.getElementById('audioFile').addEventListener('change', function(event) {
@@ -47,19 +46,3 @@ document.getElementById('audioFile').addEventListener('change', function(event) 
     });
   }
 });
-
-
-
-//Audio Player
-document.getElementById('audioFile').addEventListener('change', function(event) {
-    const file = event.target.files[0];
-    if (file) {
-      const audioPlayer = document.getElementById('audioPlayer');
-      audioPlayer.src = URL.createObjectURL(file);
-      audioPlayer.classList.remove('hidden');
-  
-      // GSAP Animations
-      gsap.fromTo(audioPlayer, { opacity: 0, y: 50 }, { opacity: 1, y: 0, duration: 1 });
-    }
-  });
-  
